8b13b4eb2ec49367fe49dacd83c3d75450447b45
Adrian Bruinhout <adrian@webfella.com.au>
2014-07-03 18:13:06 -0400

    Tidying up feed data

--- feed.js
+++ feed.js
@@ -36,19 +36,25 @@ exports.parse = function(feed, cb) {
 
 exports.filter = function(feed, cb) {
     var uid   = exports.getId(feed),
-        clean = {};
+        clean = [];
 
     _.forEach(_.first(feed.channel_guide.schedule).week, function(week) {
 
         _.forEach(week.day, function(day) {
-            var date = day.$.date;
-            clean[date] = [];
+            var date = {
+                date: day.$.date,
+                matches: []
+            };
 
             _.forEach(day.episode, function(match) {
                 if (_.first(match.series).$.id === uid && !_.isEmpty(_.first(match.title))) {
-                    clean[date].push(match);
+                    date.matches.push(exports.clean_match_data(match));
                 }
             });
+
+            if (!_.isEmpty(date.matches)) {
+                clean.push(date);
+            }
         });
     });
 
@@ -71,4 +77,23 @@ exports.getId = function(feed) {
     });
 
     return _.first(category).$.id;
+};
+
+exports.clean_match_data = function(match) {
+    var data = {
+        title: _.first(match.title),
+        duration: _.first(match.duration).$.minutes,
+        time: _.first(_.first(match["show-time"]).eastern).$,
+        status: _.first(match["show-time"]).$.live
+    };
+
+    delete match["$"];
+    delete match["title"];
+    delete match["description"];
+    delete match["series"];
+    delete match["content-rating"];
+    delete match["duration"];
+    delete match["show-time"];
+
+    return _.extend(match, data);
 };
